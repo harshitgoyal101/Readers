3b2559626226a90467c7db9aacbd2bf6802742a8
harshitgoyal101 <59048666+harshitgoyal101@users.noreply.github.com>
2021-03-16 16:20:48 +0530

    Delete minor directory

--- minor/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-admin.site.register(Reader)
-admin.site.register(Post)
-admin.site.register(Book)
-admin.site.register(Gener)
